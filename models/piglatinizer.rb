cd45e292ef03ca488b35a26f2bfaf6ddb3993c97
Richard Cho <richardcho100@gmail.com>
2019-05-15 16:47:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -24,37 +24,3 @@ class PigLatinizer
     new_word_array.join(" ")
   end
 end
-
-# class PigLatinizer
-#   def piglatinize(words)
-#     word_array = words.split(" ")
-#
-#     if word_array.count > 1
-#       ind_word = []
-#       word_array.each do |word|
-#         if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-#           ind_word << word + "way"
-#         else
-#           letter_array = word.split("")
-#           con = []
-#           until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-#             con << letter_array.shift
-#           end
-#           ind_word << letter_array.join("") + con.join("") + "ay"
-#         end
-#       end
-#       ind_word.join(" ")
-#     else
-#       if words.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-#         words + "way"
-#       else
-#         letter_array = words.split("")
-#         con = []
-#         until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-#           con << letter_array.shift
-#         end
-#         letter_array.join("") + con.join("") + "ay"
-#       end
-#     end
-#   end
-# end
