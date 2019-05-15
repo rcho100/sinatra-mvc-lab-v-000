1967ebe978a59bf647c6068c77cb7a2517a0ff29
Richard Cho <richardcho100@gmail.com>
2019-05-15 16:24:05 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -48,23 +48,41 @@ class PigLatinizer
     end
   end
 
+  def piglatinize_sentence(sentence)
+    word_array = sentence.split(" ")
+    individual_word = []
+    word_array.each do |word|
+      if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+        individual_word << word + "way"
+      else
+        letter_array = word.split("")
+        con = []
+        until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+          con << letter_array.shift
+        end
+        individual_word << letter_array.join("") + con.join("") + "ay"
+      end
+    end
+    individual_word.join(" ")
+  end
+
   def piglatinize(phrase)
     word_array = phrase.split(" ")
     if word_array.count > 1
-      ind_word = []
-      word_array.each do |word|
-        if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-          ind_word << word + "way"
-        else
-          letter_array = word.split("")
-          con = []
-          until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
-            con << letter_array.shift
-          end
-          ind_word << letter_array.join("") + con.join("") + "ay"
-        end
-      end
-      ind_word.join(" ")
+      # ind_word = []
+      # word_array.each do |word|
+      #   if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+      #     ind_word << word + "way"
+      #   else
+      #     letter_array = word.split("")
+      #     con = []
+      #     until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+      #       con << letter_array.shift
+      #     end
+      #     ind_word << letter_array.join("") + con.join("") + "ay"
+      #   end
+      # end
+      # ind_word.join(" ")
     else
       piglatinize_word(phrase)
     end
