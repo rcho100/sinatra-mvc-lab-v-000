1dd306c151bba563304b1321c10e2749228b7d24
Richard Cho <richardcho100@gmail.com>
2019-05-15 15:56:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     @pig_latinized = PigLatinizer.new
     binding.pry
-    @pig_latinized.piglatinize(params[:user_phrase])
+    @transformed_phrase = @pig_latinized.piglatinize(params[:user_phrase])
     erb :piglatinize
   end
 end
