5f15a6b3f97dd22374747b63fd59d8f01c280a31
Richard Cho <richardcho100@gmail.com>
2019-05-15 13:53:29 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+  attr_reader :phrase
+
+  def initialize
+    @phrase = phrase
+  end
+
+  def piglatinize(word)
+    binding.pry
+
+    # consonants = [B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z,]
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    if
+
+    else
+
+    end
+  end
+
+end
