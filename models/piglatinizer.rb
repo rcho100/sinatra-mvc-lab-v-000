2f80b454ceb46a9ed7aedc7ab1823667e7f4c5db
Richard Cho <richardcho100@gmail.com>
2019-05-15 16:40:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -39,6 +39,15 @@
 
 
 class PigLatinizer
+  def piglatinize(input_string)
+    word_array = input_string.split(" ")
+    if word_array.count > 1
+      piglatinize_sentence(input_string)
+    else
+      piglatinize_word(input_string)
+    end
+  end
+
   def piglatinize_word(word)
     if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
       word = word + "w"
@@ -56,18 +65,7 @@ class PigLatinizer
   def piglatinize_sentence(sentence)
     word_array = sentence.split(" ")
     new_word_array = []
-    word_array.each do |word|
-      new_word_array << piglatinize_word(word)
-    end
+    word_array.each {|word| new_word_array << piglatinize_word(word)}
     new_word_array.join(" ")
-  end
-
-  def piglatinize(input_string)
-    word_array = input_string.split(" ")
-    if word_array.count > 1
-      piglatinize_sentence(input_string)
-    else
-      piglatinize_word(input_string)
-    end
   end
 end
