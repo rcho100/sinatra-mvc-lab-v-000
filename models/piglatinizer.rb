2e62768de4f7dd05e0bf08a1bd3eabe86ced3dde
Richard Cho <richardcho100@gmail.com>
2019-05-15 16:10:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,39 @@
+# class PigLatinizer
+#   def piglatinize(words)
+#     word_array = words.split(" ")
+#
+#     if word_array.count > 1
+#       ind_word = []
+#       word_array.each do |word|
+#         if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+#           ind_word << word + "way"
+#         else
+#           letter_array = word.split("")
+#           con = []
+#           until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+#             con << letter_array.shift
+#           end
+#           ind_word << letter_array.join("") + con.join("") + "ay"
+#         end
+#       end
+#       ind_word.join(" ")
+#     else
+#       if words.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+#         words + "way"
+#       else
+#         letter_array = words.split("")
+#         con = []
+#         until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+#           con << letter_array.shift
+#         end
+#         letter_array.join("") + con.join("") + "ay"
+#       end
+#     end
+#   end
+# end
+
+
+
 class PigLatinizer
   def piglatinize(words)
     word_array = words.split(" ")
@@ -30,4 +66,19 @@ class PigLatinizer
       end
     end
   end
+
+  def piglatinize_word(word)
+    if word.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+      word + "way"
+    else
+      letter_array = word.split("")
+      con = []
+      until letter_array.first.match(/\A(a|A|e|E|i|I|o|O|u|U|y|Y)/)
+        con << letter_array.shift
+      end
+      letter_array.join("") + con.join("") + "ay"
+
+  end
+
+
 end
