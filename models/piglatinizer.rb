b198a4ad30c8073b2f2365f3802515c7fad13b59
Richard Cho <richardcho100@gmail.com>
2019-05-15 13:56:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,15 +6,15 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-    binding.pry
-
-    # consonants = [B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z,]
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-    if
-
-    else
-
-    end
+    # binding.pry
+    # #
+    # # # consonants = [B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z,]
+    # # vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    # # if
+    # #
+    # # else
+    # #
+    # # end
   end
 
 end
